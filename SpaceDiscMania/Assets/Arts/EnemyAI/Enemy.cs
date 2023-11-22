697792108ce1f30516704c716ef0e3ef909eab2d
Anders <ande9823@gmail.com>
2023-11-22 14:55:46 +0100

    Fixed all but one merge error with EnemyAI Controller
    Assets/Arts/EnemyAI/EnemyAI.controller Skal skiftes ud, men jeg har ikke objektet til at fikse det.

--- SpaceDiscMania/Assets/Arts/EnemyAI/Enemy.cs
+++ SpaceDiscMania/Assets/Arts/EnemyAI/Enemy.cs
@@ -1,56 +1,3 @@
-<<<<<<< HEAD:SpaceDiscMania/Assets/Arts/EnemyAI/Enemy.cs
-using UnityEngine;
-using System.Collections;
-
-public class Enemy : MonoBehaviour
-{
-    public int enemyHP = 100;
-    public GameObject projectile;
-    public Transform projectilePoint;
-
-    public bool hasShot = false;
-    public int shotDelay = 5;
-
-    public Animator animator;
-
-    public void Shoot()
-    {
-        if (hasShot == false) {
-            hasShot = true;
-            GameObject projectileObj = Instantiate(projectile, projectilePoint.position, Quaternion.identity);
-            Rigidbody rb = projectileObj.GetComponent<Rigidbody>();
-
-            rb.AddForce(transform.forward * 30f, ForceMode.Impulse);
-            rb.AddForce(transform.up * 7, ForceMode.Impulse);
-
-            Invoke("Reload", shotDelay);
-            StartCoroutine(DestroyProjectile(projectileObj));
-        }
-    }
-
-    public void Reload() {
-        hasShot = false;
-    }
-    private IEnumerator DestroyProjectile(GameObject projectile) {
-        yield return new WaitForSeconds(5);
-        Destroy(projectile);
-    }
-
-    public void TakeDamage(int damageAmount)
-    {
-        enemyHP -= damageAmount;
-        if(enemyHP <= 0)
-        {
-            animator.SetTrigger("death");
-            GetComponent<CapsuleCollider>().enabled = false;
-        }
-        else
-        {
-            animator.SetTrigger("damage");
-        }
-    }
-}
-=======
 using UnityEngine;
 using System.Collections;
 
@@ -120,4 +67,3 @@ public class Enemy : MonoBehaviour
         }
     }
 }
->>>>>>> cecf62c90914945bf5cb6bad4192fa10e5b8bac1:SpaceDiscMania/Assets/EnemyAI/Enemy.cs
--- SpaceDiscMania/Assets/Arts/EnemyAI/Projectile.cs
+++ SpaceDiscMania/Assets/Arts/EnemyAI/Projectile.cs
@@ -1,40 +1,3 @@
-<<<<<<< HEAD:SpaceDiscMania/Assets/Arts/EnemyAI/Projectile.cs
-using System.Collections;
-using UnityEngine;
-
-public class Projectile : MonoBehaviour
-{
-    public GameObject impactEffect;
-    public int knockback = 5;
-    public float radius = 3;
-    public int damageAmount = 15;
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        
-        GameObject impact  = Instantiate(impactEffect, transform.position, Quaternion.identity);
-        Destroy(impact, 2);
-
-        if(collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Player")) {
-            Rigidbody enemy = collision.gameObject.GetComponent<Rigidbody>();
-            Vector3 difference = enemy.transform.position - transform.position;
-            difference = difference.normalized * knockback;
-            enemy.AddForce(difference, ForceMode.Impulse);
-        }
-        /*
-        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
-        foreach(Collider nearbyObject in colliders)
-        {
-            if(nearbyObject.tag == "Player")
-            {
-                StartCoroutine(FindObjectOfType<PlayerManager>().TakeDamage(damageAmount));
-            }
-        }
-        this.enabled = false;
-        */
-    }
-}
-=======
 using System.Collections;
 using UnityEngine;
 
@@ -53,10 +16,10 @@ public class Projectile : MonoBehaviour
         Destroy(impact, 2);
 
         if(collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Player")) {
-            Debug.Log(""+collision.gameObject.name);
-            
+            Debug.Log("" + collision.gameObject.name);
+
             Rigidbody enemy = collision.gameObject.GetComponent<Rigidbody>();
-            Vector3 difference = enemy.transform.position - this.transform.position;
+            Vector3 difference = enemy.transform.position - transform.position;
             difference = difference.normalized * knockback;
             enemy.AddForce(difference, ForceMode.Impulse);
         }
@@ -73,4 +36,3 @@ public class Projectile : MonoBehaviour
         */
     }
 }
->>>>>>> cecf62c90914945bf5cb6bad4192fa10e5b8bac1:SpaceDiscMania/Assets/EnemyAI/Projectile.cs
